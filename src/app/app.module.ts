8a147abd57c265c2fc39df1ba916ecf9ea167291
alexandremessang <alexandre.messang@gmail.com>
2021-01-29 10:08:06 +0100

    add tree component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TreeComponent } from './tree/tree.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      TreeComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
