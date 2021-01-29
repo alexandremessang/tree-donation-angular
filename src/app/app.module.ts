476928afb53a45e2bc06d802d586ab215037ba0e
Augustin ANTOINE <augustin.antoine.pro@gmail.com>
2021-01-29 11:04:22 +0100

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TreeComponent } from './tree/tree.component';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  declarations: [	
+  declarations: [
     AppComponent,
       TreeComponent
    ],
