0ef5a48cf2be5947997b2b30d92b48274e37d1de
alexandremessang <alexandre.messang@gmail.com>
2021-01-29 11:01:42 +0100

    add toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TreeComponent } from './tree/tree.component';
+import { MatButtonModule } from '@angular/material/button/';
+import { MatToolbarModule } from '@angular/material/toolbar/';
+
 
 @NgModule({
-  declarations: [	
+  declarations: [
     AppComponent,
-      TreeComponent
-   ],
+      TreeComponent,
+    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
